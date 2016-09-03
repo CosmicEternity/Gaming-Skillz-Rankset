feca9606fd40f10f661b01d6982b3ee245cc8d44
GamesWithIsaac <isavere@outlook.com>
2016-09-03 17:47:41 -0400

    Add files via upload

---
+++ rankset.php
@@ -0,0 +1,16 @@
+<?php
+
+$ranksetNames["Gaming Skillz"] = "Gaming Skillz";
+
+$ranksetData["Gaming Skillz"] = array(
+array("num" => 0, "image" => '', "text" => 'Non-Gamer'),  
+array("num" => 1, "image" => '', "text" => 'Newbie'), 
+array("num" => 10, "image" => '', "text" => 'Amateur Gamer'),
+array("num" => 50, "image" => '', "text" => 'Decent Player'),
+array("num" => 100, "image" => '', "text" => 'Intermediate Gamer'), 
+array("num" => 150, "image" => '', "text" => 'Well known Gamer'),
+array("num" => 250, "image" => '', "text" => 'Expert Gamer'),   
+array("num" => 500, "image" => '', "text" => 'Master Gamer'),
+array("num" => 750, "image" => '', "text" => 'Veteran Gamer'), 
+array("num" => 1000, "image" => '', "text" => 'Ultra Gamer'),                                                                                                                                                                                                                                                                                       
+);
