0b30c715cbcfaae1023e640f5f7975c4e154d436
GamesWithIsaac <isavere@outlook.com>
2016-09-13 17:02:42 -0400

    Update rankset.php
    wake me up inside!(cant wake up).png added
    
    d-dont ask, anyways I added some new things

--- rankset.php
+++ rankset.php
@@ -12,5 +12,19 @@ array("num" => 150, "image" => '', "text" => 'Well known Gamer'),
 array("num" => 250, "image" => '', "text" => 'Expert Gamer'),   
 array("num" => 500, "image" => '', "text" => 'Master Gamer'),
 array("num" => 750, "image" => '', "text" => 'Veteran Gamer'), 
-array("num" => 1000, "image" => '', "text" => 'Ultra Gamer'),                                                                                                                                                                                                                                                                                       
+array("num" => 1000, "image" => '', "text" => 'Ultra Gamer'),                
+array("num" => 1100, "image" => '', "text" => 'Mega Gamer'),
+array("num" => 1200, "image" => '', "text" => 'Explosive Gamer'),
+array("num" => 1400, "image" => '', "text" => 'Epic Gamer'),
+array("num" => 1600, "image" => '', "text" => 'Super Gamer'),\
+array("num" => 1800, "image" => '', "text" => 'Dank Gamer'),
+array("num" => 2000, "image" => '', "text" => 'Doge Gamer'),
+array("num" => 2200, "image" => '', "text" => 'LEL Gamer'),
+array("num" => 2400, "image" => '', "text" => 'Unicorn Gamer'),
+array("num" => 2800, "image" => '', "text" => 'Cow Gamer'),
+array("num" => 2, "image" => '', "text" => ' '),
+
+
+
+
 );
