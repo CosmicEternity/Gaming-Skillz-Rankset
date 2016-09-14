b09e780a0e37e6aaa9237cc7af69285b7df319e5
GamesWithIsaac <isavere@outlook.com>
2016-09-14 16:29:01 -0400

    Updated rankset.php
    changed the decent Player to Decent Gamer

--- rankset.php
+++ rankset.php
@@ -6,7 +6,7 @@ $ranksetData["Gaming Skillz"] = array(
 array("num" => 0, "image" => '', "text" => 'Non-Gamer'),  
 array("num" => 1, "image" => '', "text" => 'Newbie'), 
 array("num" => 10, "image" => '', "text" => 'Amateur Gamer'),
-array("num" => 50, "image" => '', "text" => 'Decent Player'),
+array("num" => 50, "image" => '', "text" => 'Decent Gamer'),
 array("num" => 100, "image" => '', "text" => 'Intermediate Gamer'), 
 array("num" => 150, "image" => '', "text" => 'Well known Gamer'),
 array("num" => 250, "image" => '', "text" => 'Expert Gamer'),   
